6a9efa99b1b60316eb4abc4b2083b392c401fb6f
Jayesh-Jain31 <jayeshjain66192@gmail.com>
2025-07-15 05:14:03 +0530

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,20 @@
+// data/products.js
+const products = [
+  {
+    name: 'Banarasi Silk Saree',
+    price: 1499,
+    image: 'https://example.com/images/saree1.jpg',
+  },
+  {
+    name: 'Kanchipuram Saree',
+    price: 1799,
+    image: 'https://example.com/images/saree2.jpg',
+  },
+  {
+    name: 'Cotton Printed Saree',
+    price: 899,
+    image: 'https://example.com/images/saree3.jpg',
+  },
+];
+
+export default products;
